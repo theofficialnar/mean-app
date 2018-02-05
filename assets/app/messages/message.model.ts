30a08ee7d80e8b90816802c1deb3aa70f52d7fb6
Nar Cuenca <narc.ph@gmail.com>
2018-02-05 11:03:45 +0800

    merge fix

--- assets/app/messages/message.model.ts
+++ assets/app/messages/message.model.ts
@@ -1,10 +1,10 @@
 export class Message {
-  content: String;
-  username: String;
-  messageId?: String;
-  userId?: String;
+  content: string;
+  username: string;
+  messageId?: string;
+  userId?: string;
 
-  constructor (content: String, username: String, messageId?: String, userId?: String) {
+  constructor (content: string, username: string, messageId?: string, userId?: string) {
     this.content = content;
     this.username = username;
     this.messageId = messageId;
