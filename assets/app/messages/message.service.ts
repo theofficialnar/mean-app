9f79326306d2abf9aed87bcccbb6e6f3d12131f4
Nar Cuenca <narc.ph@gmail.com>
2018-02-14 14:33:24 +0800

    added delete functionality on front and back end

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -70,8 +70,13 @@ export class MessageService {
       .map((response: Response) => response.json())
       .catch((error: Response) => Observable.throw(error.json()));
   }
-
+  
+  //deletes the message from the server
   deleteMessage (message: Message) {
     this.messages.splice(this.messages.indexOf(message), 1);
+    return this.http
+    .delete(`http://localhost:3000/message/${message.messageId}`)
+    .map((response: Response) => response.json())
+    .catch((error: Response) => Observable.throw(error.json()));
   }
 }
--- routes/messages.js
+++ routes/messages.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { ObjectID } = require('mongoDB');
 
 const Message = require('../models/message');
 
@@ -41,27 +42,96 @@ router.post('/', async (req, res) => {
 
 //update message
 router.patch('/:id', async (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).json({
+      title: 'An error occured',
+      errorMsg: {message: 'ID not found'}
+    })
+  };
+
   try {
-    const message = await Message.findById(req.params.id);
+    const message = await Message.findOneAndUpdate({
+      _id : id
+    }, {
+      $set : {content : req.body.content}
+    }, {
+      new : true
+    });
+
     if (!message) {
       return res.status(404).json({
         title: 'No message retrieved',
         errorMsg: {message: 'Message not found'}
       })
     };
-    message.content = req.body.content;
-    try {
-      const result = await message.save();
-      res.status(200).json({
-        title: 'Message updated',
-        obj: result
-      });
-    } catch (error) {
-      res.status(500).json({
-        title: 'An error occured',
-        errorMsg: error
-      });
+
+    res.status(200).json({
+      title: 'Message updated',
+      obj: message
+    });
+  } catch (error) {
+    res.status(500).json({
+      title: 'An error occured',
+      errorMsg: error
+    });
+  }
+
+  // try {
+  //   const message = await Message.findById(id);
+  //   if (!message) {
+  //     return res.status(404).json({
+  //       title: 'No message retrieved',
+  //       errorMsg: {message: 'Message not found'}
+  //     })
+  //   };
+  //   message.content = req.body.content;
+  //   try {
+  //     const result = await message.save();
+  //     res.status(200).json({
+  //       title: 'Message updated',
+  //       obj: result
+  //     });
+  //   } catch (error) {
+  //     res.status(500).json({
+  //       title: 'An error occured',
+  //       errorMsg: error
+  //     });
+  //   };
+  // } catch (error) {
+  //   res.status(500).json({
+  //     title: 'An error occured',
+  //     errorMsg: error
+  //   });
+  // };
+});
+
+// delete message
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).json({
+      title: 'An error occured',
+      errorMsg: {message: 'ID not found'}
+    })
+  };
+
+  try {
+    const message = await Message.findOneAndRemove({
+      _id : id
+    });
+
+    if (!message) {
+      return res.status(404).json({
+        title: 'No message deleted',
+        errorMsg: {message: 'Message not found'}
+      })
     };
+
+    res.status(200).json({
+      title: 'Message deleted',
+      obj: message
+    });
   } catch (error) {
     res.status(500).json({
       title: 'An error occured',
